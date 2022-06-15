3edb03d41031c379e66c73a5fef461dc0506dc85
kcristian <cristian.cruz.a66@gmail.com>
2022-06-15 20:11:16 -0300

    agregue un comentario

--- src/Sala.java
+++ src/Sala.java
@@ -3,12 +3,18 @@ import java.util.ArrayList;
 public class Sala {
 	
 	private int num_sala;
+	public static int num_sala_siguiente=1;
 	private int capacidad;
 	private ArrayList<Asiento> asientos;
 	
-	public Sala(int num_sala,int capacidad){
+	public Sala(int capacidad){
 		
-		this.num_sala=num_sala;
+		this.num_sala=num_sala_siguiente;
+		num_sala_siguiente++;
+		this.capacidad=capacidad;
+		for(int i=0;i<this.capacidad;i++){
+			asientos.add()// creando asientos
+		}
 		
 	}
 }
